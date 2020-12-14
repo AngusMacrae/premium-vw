278485c277d01c353917d9a068b4bdd40264761a
Angus Macrae <angusdmacrae@gmail.com>
2020-12-14 01:48:08 +0000

    Add basic lightbox for opening gallery images for users with JS enabled

--- js/script.js
+++ js/script.js
@@ -12,4 +12,39 @@ galleryFilterSelect.addEventListener("change", function () {
   });
 });
 
+(function replaceAnchorsWithClickableDivs() {
+  const lightboxOverlay = document.getElementById("lightbox-overlay");
+  const lightboxImg = document.getElementById("lightbox-img");
+
+  function openLightbox(url) {
+    lightboxImg.src = url;
+    lightboxOverlay.hidden = false;
+  }
+  
+  function closeLightbox() {
+    lightboxOverlay.hidden = true;
+  }
+
+  let galleryImageAnchors = document.querySelectorAll(".gallery-img-anchor")
+  const len = galleryImageAnchors.length;
+  
+  for (let i = len - 1; i > -1; i--) {
+    let temp = galleryImageAnchors[i];
+    let newList = temp.outerHTML;
+    newList = newList.replace('<a ','<div ');
+    newList = newList.replace('</a>','</div>');
+    temp.outerHTML = newList;
+  }
+
+  galleryImageAnchors = document.querySelectorAll(".gallery-img-anchor")
+
+  galleryImageAnchors.forEach((node) => {
+    node.addEventListener("click", function () {
+      openLightbox(this.querySelector("img").src);
+    })
+  });
+
+  lightboxOverlay.addEventListener("click", closeLightbox);
+})();
+
 AOS.init();
